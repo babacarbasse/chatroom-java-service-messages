bb632b3217e21837a2f43af2fc75ea1b02a85580
Babacar Basse <babacar.basse@esp.sn>
2019-04-23 22:21:41 +0000

    remove method getMessages

--- src/com/esp/chatroom/Main.java
+++ src/com/esp/chatroom/Main.java
@@ -1,14 +1,9 @@
 package com.esp.chatroom;
 
-import java.util.ArrayList;
-
 import org.apache.xmlrpc.WebServer;
 
 
-
 public class Main {
-
-	static ArrayList<String> messages;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -16,7 +11,6 @@ public class Main {
 			System.out.println("Attempting to start XML-RPC Server...");
 			WebServer server = new WebServer(4041);
 			(server).addHandler("message", new Main());
-			messages = new ArrayList<String>();
 			server.start();
 			System.out.println("Started successfully.");
 			System.out.println("Accepting requests. (Halt program to stop.)");
@@ -27,11 +21,7 @@ public class Main {
 	public String postMessage(String pseudo, String newMessage) {
         String displayMessage = pseudo + " >>> " + newMessage;
         System.out.println(displayMessage);
-        messages.add(displayMessage);
         return displayMessage;
     }
 
-	public ArrayList<String> getMessages(){
-		return messages;
-	}
 }
