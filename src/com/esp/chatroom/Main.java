059a51b29326420447a4d54200cb77f8c1e30584
Babacar Basse <babacar.basse@esp.sn>
2019-04-23 19:57:08 +0000

    init project and create method postMessage and getMessage

---
+++ src/com/esp/chatroom/Main.java
@@ -0,0 +1,34 @@
+package com.esp.chatroom;
+
+import org.apache.xmlrpc.WebServer;
+
+
+
+public class Main {
+
+	String message = "";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try { 
+			System.out.println("Attempting to start XML-RPC Server...");
+			WebServer server = new WebServer(4041);
+			(server).addHandler("message", new Main());
+			server.start();
+			System.out.println("Started successfully.");
+			System.out.println("Accepting requests. (Halt program to stop.)");
+		}
+		catch (Exception exception){ System.err.println("JavaServer: " + exception); }
+	}
+	
+	public String postMessage(String pseudo, String newMessage) {
+        String displayMessage = pseudo + " >>> " + newMessage;
+        System.out.println(displayMessage);
+        message = displayMessage;
+        return displayMessage;
+    }
+
+	public String getMessage(){
+		return message;
+	}
+}
